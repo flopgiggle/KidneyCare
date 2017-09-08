b69a6922e452bc927f4fe68e2255fbb9094559c6
liwenhai <340416478@qq.com>
2017-09-08 15:59:13 +0800

    fixbug

--- pages/my/my.js
+++ pages/my/my.js
@@ -14,7 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      this.setData({ data: app.globalData.user});
+      
   },
   onConsultingTap: function (e) {
       wx.navigateTo({
@@ -38,7 +38,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+      this.setData({ userInfo: app.globalData.user });
   },
 
   /**
